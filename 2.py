0c4289880e868489cdce35b204ab2e291449d13d
Aditya Yadav <adi.yadav.021504@gmail.com>
2025-07-19 10:13:14 +0530

    BASIC PYTHON -- DATA TYPES TO LOOPS AND FUNCTION

---
+++ 2.py
@@ -0,0 +1,18 @@
+print("Data types ,condition,loops and function")
+a=10            #int
+b=3.14          #float
+c='Your_Name'   #str
+d=True          #bool
+e=[1,2,3]       #list
+f=(4,5)         #tuple
+g={"x":1}       #dict
+h={1,2,3}       #set
+
+print(type(a),a)
+print(type(b),b)
+print(type(c),c)
+print(type(d),d)
+print(type(e),e)
+print(type(f),f)
+print(type(g),g)
+print(type(h),h)
